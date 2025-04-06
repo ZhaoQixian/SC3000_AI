63cb0521fc3acf176c47d1e7302f67c75bcd6c2f
ZhaoQixian <zqx2467027771@gmail.com>
2025-04-07 03:26:00 +0800

    FEAT: update smart phone technology to be a general form of business, and galactica_s3 is an instance

--- Project-2/qn_1_1.pl
+++ Project-2/qn_1_1.pl
@@ -1,7 +1,23 @@
-competitor(sumsum,appy).
-developed_smart_phone(sumsum,galactica-s3).
-stole(galactica-s3,stevey).
-boss(appy,stevey).
+% Facts
+competitor(sumsum, appy).
+developed_smart_phone(sumsum, galactica_s3).
+steal(stevey, galactica_s3).
+boss(stevey, appy).
+smart_phone_technology(galactica_s3).
 
-unethical(X) :- boss(Company,X), competitor(Competitor,Company),developed_smart_phone(Competitor,Technology),stole(Technology,X).
+% Rules
+business(T) :-
+    smart_phone_technology(T).
 
+unethical(X) :-
+    boss(X, C),
+    steal(X, T),
+    developed_smart_phone(Y, T),
+    competitor(Y, C),
+    business(T).
+
+rival(Y, C) :-
+    competitor(Y, C).
+
+% Query
+% ?- unethical(stevey).
