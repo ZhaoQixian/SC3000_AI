73845e691f7298cd5777baa923d8e0362c593235
ZhaoQixian <zqx2467027771@gmail.com>
2025-04-07 16:36:56 +0800

    CHORE: change method 2 name

--- Project-2/qn_2_2_method2.pl
+++ Project-2/qn_2_2_method2.pl
@@ -1,37 +1,27 @@
-% ------------------------------
 % Declare dead/1 as dynamic so that it exists even if no clauses are defined.
-% ------------------------------
 :- dynamic dead/1.
 
-% ------------------------------
 % Facts: Parent-child relationships
-% ------------------------------
 parent(queen_elizabeth, prince_charles).
 parent(queen_elizabeth, princess_ann).
 parent(queen_elizabeth, prince_andrew).
 parent(queen_elizabeth, prince_edward).
 
-% ------------------------------
 % Facts: Gender definitions
 % (These remain in case they are needed for other queries.)
-% ------------------------------
 male(prince_charles).
 male(prince_andrew).
 male(prince_edward).
 female(princess_ann).
 
-% ------------------------------
 % Facts: Numeric birth order
 % Lower numbers indicate earlier birth.
-% ------------------------------
 birth_order(prince_charles, 1).
 birth_order(princess_ann, 2).
 birth_order(prince_andrew, 3).
 birth_order(prince_edward, 4).
 
-% ------------------------------
 % Life Status
-% ------------------------------
 % For example, to declare Prince Andrew as deceased, you would uncomment the line below:
 % dead(prince_andrew).
 %
@@ -39,18 +29,14 @@ birth_order(prince_edward, 4).
 alive(X) :-
   \+ dead(X).
 
-% ------------------------------
 % Eligible Succession Members
-% ------------------------------
 % Only living children of a given Parent are considered in the succession.
 succession_member(Parent, Child) :-
     parent(Parent, Child),
     alive(Child).
 
-% ------------------------------
 % Old Succession Rule (for reference)
 % Males inherit first, then females (ordered by birth order).
-% ------------------------------
 old_succession_rule(Parent, Succession) :-
     % Collect all living male heirs with their birth order as Key-Value pairs.
     findall(Order-Child,
@@ -73,10 +59,8 @@ old_succession_rule(Parent, Succession) :-
     % Append the male heirs first, then female heirs.
     append(SortedMales, SortedFemales, Succession).
 
-% ------------------------------
 % New Succession Rule:
 % The throne is passed solely according to the order of birth, irrespective of gender.
-% ------------------------------
 new_succession_rule(Parent, Succession) :-
     % Collect all living children with their birth order as Key-Value pairs.
     findall(Order-Child,
@@ -93,9 +77,7 @@ extract_children([], []).
 extract_children([_-Child|RestPairs], [Child|RestChildren]) :-
     extract_children(RestPairs, RestChildren).
 
-% ------------------------------
 % Example Queries:
-% ------------------------------
 % 1. To get the old line of succession for Queen Elizabeth:
 %    ?- old_succession_rule(queen_elizabeth, Succession).
 %
